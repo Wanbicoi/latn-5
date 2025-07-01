8491076aea01bd2d199821f5d45ae0768e3872a5
trung <hoang>
2025-07-01 18:53:08 +0700

    refactor: simplify members view by removing specific user exclusions

--- supabase/views/members.sql
+++ supabase/views/members.sql
@@ -8,7 +8,4 @@ select
 FROM
     public_v2._users u
 WHERE
-    u.id not in (
-        '8a3a1c67-c14b-4ae6-a3ac-4f54e4c0224b',
-        '9cd38964-fe23-4596-aacd-c6b2de77a000'
-    )
+    u.is_system = false
