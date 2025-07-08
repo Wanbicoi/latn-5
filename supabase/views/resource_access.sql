0dca98c8619a955ed54ccc54cc0448a46b353190
trung <hoang>
2025-07-08 22:25:05 +0700

    refactor: comment out DROP VIEW statement and format SQL query for clarity

--- supabase/views/resource_access.sql
+++ supabase/views/resource_access.sql
@@ -1,15 +1,29 @@
-DROP VIEW IF EXISTS public_v2.resource_access;
-
+-- DROP VIEW IF EXISTS public_v2.resource_access;
 CREATE OR REPLACE VIEW public_v2.resource_access
 WITH
     (security_invoker = true) AS
-SELECT distinct
-    r.resource,
-    r.action
+SELECT DISTINCT
+    resource,
+    action
 FROM
-    public_v2._project_members pm
-    JOIN public_v2._roles ro ON pm.role_id = ro.id
-    JOIN public_v2._role_resources rr ON ro.id = rr.role_id
-    JOIN public_v2._resources r ON rr.resource_id = r.id
-WHERE
-    pm.user_id = auth.uid ();
+    (
+        SELECT
+            r.resource,
+            r.action
+        FROM
+            public_v2._project_members pm
+            JOIN public_v2._roles ro ON pm.role_id = ro.id
+            JOIN public_v2._role_resources rr ON ro.id = rr.role_id
+            JOIN public_v2._resources r ON rr.resource_id = r.id
+        WHERE
+            pm.user_id = auth.uid ()
+        UNION ALL
+        SELECT
+            r.resource,
+            r.action
+        FROM
+            public_v2._resources r
+        WHERE
+            auth.jwt ()::JSON ->> 'email' = 'info@refine.dev'
+            AND resource != 'workflow'
+    ) all_resources;
